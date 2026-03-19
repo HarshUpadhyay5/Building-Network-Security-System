b6c4ba82a80317431b2c3b03e187486b3ce5a225
HarshUpadhyay5 <harshupadhyayofficial5@gmail.com>
2026-03-19 11:52:03 +0530

    ETL pipeline Created

---
+++ push_data.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
+
+import certifi
+ca  = certifi.where()
+
+import pandas as pd
+import numpy as np
+import pymongo
+from Networksecurity.Exception.exception import NetworkSecurityException
+from Networksecurity.Logging.logger import logging
+
+
+class NetworkDataExtract() :
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def csv_to_json_convertor(self , file_path):
+        try:
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True , inplace= True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def insert_data_mongodb(self , records , database , collection) :
+        try:
+            self.database = database
+            self.records = records
+            self.collection = collection
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+
+            self.collection.insert_many(self.records)
+
+            return (len(self.records))
+        
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+if __name__ == "__main__":
+    File_path = "Network_Data\phisingData.csv"
+    DATABASE  = "HARSH_5"
+    Collection = "NetworkData"
+    Networkobj = NetworkDataExtract()
+    records = Networkobj.csv_to_json_convertor(file_path=File_path)
+    print(records)
+    no_of_records = Networkobj.insert_data_mongodb(records=records , database= DATABASE , collection= Collection)
+    print(no_of_records)
